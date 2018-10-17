ad56440668f39d170ee868545b061b3ff8fd9b35
xiexu <jackycher@126.com>
2018-10-17 14:57:09 +0800

    run

--- testExcel.py
+++ testExcel.py
@@ -1,11 +1,9 @@
 from py2neo import Node, Relationship, Graph
 import pandas as pd
 import ast
-import numpy as np
-import re
-import os
 
 
+# 创建图数据库连接
 graph = Graph("bolt://localhost:7687", username="neo4j", password="1039")
 
 # 删除数据库原数据
@@ -17,55 +15,57 @@ root = Node("Class", name="Thing")
 tx.create(root)
 tx.commit()
 
+# 读入data数据表（不要表头）
 data = pd.read_excel("data333.xls", header=None)
+
+# 补充第1-3列key值及相应符号
 data[0] = "{url:"+data[0]+"}"
 data[1] = "{name:"+data[1]+"}"
 data[2] = "{简介:"+data[2]+"}"
 
+# 使用正则表达式处理"(替换成',已保证以后添加的"唯一成对)、换行符(去除)、以及在开头和结尾的{}符号(添加")
 repdic = {'\"': '\'', '\n': '', '^{': '{\"', '}$': '\"}'}
 data1 = data.replace(repdic, regex=True)
-
 
 # print(data1.index)
 # print(data1.shape)
 # print(data1.iloc)
 # print(data1.loc)
 
-
+# 按行遍历data1中数据
 for indexs in data1.index:
-    #     print(indexs)
-    #    print(type(data1.loc[indexs]))
+    print("正在处理第", indexs+1, "行....")
 
+    # 丢弃data1一行中NaN（避免使用split函数中的类型错误
     notes = data1.loc[indexs].dropna()
-    #  print("dropna")
+
+    # 处理一行一列中数据的:问题
+    # 首先将数据按:符号分割成数组
+    # 然后将第一个:符号前的数据添加":"，再合并以后的数组数据并补充原:符号
+    # 目的是保证将第一个:替换成":"，其他不变
     notestmp = notes.map(lambda x: x.split(":"))
     notes = notestmp.map((lambda x: x[0] + '\":\"')) + notestmp.map(lambda x: ':'.join(x[1:]))
-    notes = notes.str.replace('\n', '')
+
+    # 定义一个空字典，以保证后面使用.update方法合并字典数据
     prodic = {}
-    print(type(prodic))
+
+    # 遍历一行中(已删除NaN)的每个格数据
     for values1 in notes.index:
 
-        print(values1)
-        print(type(notes.loc[values1]))
-        print(notes.loc[values1])
-
+        # 将数据类型转换为dic
         notes1 = ast.literal_eval(notes.loc[values1])
-        print(type(notes1))
+        # 将每个中数据dic合并
         prodic.update(**notes1)
-        print(type(ast.literal_eval(notes.loc[values1])))
 
+    # 开始写入图数据库
     tx = graph.begin()
+    # 定义节点，并将原每行的数据合并成的字典作为属性参数传入
     ent = Node("Entity", **prodic)
+    # 定义类到实体、实体到类的关系
     re1 = Relationship(root, "实体", ent)
     re2 = Relationship(ent, "类", root)
+    # 建立节点和关系并提交数据库
     tx.create(ent)
     tx.create(re1)
     tx.create(re2)
     tx.commit()
-    
-
-
-
-
-
-
